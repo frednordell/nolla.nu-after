32b06b9075c2e635aacd246f1d7d88ed8aa355d3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-12 20:56:25 +0000

    Deploy frednordell/nolla.nu-after to github.com/frednordell/nolla.nu-after.git:release

---
+++ precache-manifest.c6530edb03270bbc8a1c06fdee5e93b4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a034e8ca97d2f2352e61cceaf227cb25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7050fa5c28bc77746cb8",
+    "url": "/static/css/main.6fc0cfdb.chunk.css"
+  },
+  {
+    "revision": "19b42678b9cab02f26f6",
+    "url": "/static/js/2.55f97433.chunk.js"
+  },
+  {
+    "revision": "7050fa5c28bc77746cb8",
+    "url": "/static/js/main.1983596e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5860f2fdfda82f34d0afd614bfda89df",
+    "url": "/static/media/airom.5860f2fd.jpg"
+  },
+  {
+    "revision": "4f713088ca28b7e3ece7ef4b8fa23d6b",
+    "url": "/static/media/azula.4f713088.jpg"
+  },
+  {
+    "revision": "ffd44439058a44de0941b1ee560b4d16",
+    "url": "/static/media/drag.ffd44439.jpg"
+  },
+  {
+    "revision": "01141c2e5934302fa7f4331123b55392",
+    "url": "/static/media/ili.01141c2e.jpg"
+  },
+  {
+    "revision": "7bf468b1da98d73606e7cf9355f3c12a",
+    "url": "/static/media/ite.7bf468b1.jpg"
+  },
+  {
+    "revision": "5ed3a700f40aec905cc1bc95e11740c8",
+    "url": "/static/media/kfs.5ed3a700.jpg"
+  },
+  {
+    "revision": "0f07416b3b3dabff2387faee28bc1984",
+    "url": "/static/media/kk.0f07416b.png"
+  },
+  {
+    "revision": "e0a3d8d50cc12f7869aa425bdb982022",
+    "url": "/static/media/start.e0a3d8d5.png"
+  }
+]);
